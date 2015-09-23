cd5b20ed1e9c0236c1e1d263278372f0bdc5dbff
Jessica Joly & Andrew Haupt <student@epicodus.com>
2015-09-23 10:56:54 -0700

    Add post-tile.hbs

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'caffeine-blog',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https:///transcendingcoffee.firebaseio.com/',
+    firebase: 'https://transcendingcoffee.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
