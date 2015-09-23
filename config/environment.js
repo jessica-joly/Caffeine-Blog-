5f9169d9bfc6e382233c5fa18cd62b7ab10750cd
Jessica Joly & Andrew Haupt <student@epicodus.com>
2015-09-23 10:16:04 -0700

    Add seed.JSON file and upload to FIREBASE.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'caffeine-blog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https:///transcendingcoffee.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
