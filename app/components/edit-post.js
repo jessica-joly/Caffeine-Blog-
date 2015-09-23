6b3d78cee7c3049a16d1f67bd153bc380dc07b02
Jessica Joly & Andrew Haupt <student@epicodus.com>
2015-09-23 16:45:38 -0700

    Add update functionality to post route.

--- app/components/edit-post.js
+++ app/components/edit-post.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateShowing: false,
   actions: {
+    updateShow() {
+      this.set('updateShowing', true);
+    },
     deletePost(model) {
       if (confirm('Are you sure you want to delete this post?')) {
         this.sendAction('destroyPost', model);
       }
+    },
+    update(model) {
+      var params = {
+        title: this.get('title'),
+        user: this.get('user'),
+        body: this.get('body'),
+        image: this.get('image'),
+      };
+      this.sendAction('update', model, params);
+      this.set('updateShowing', false);
     }
   }
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -8,6 +8,16 @@ export default Ember.Route.extend({
     destroyPost(model) {
       model.destroyRecord();
       this.transitionTo('index');
+    },
+
+    update(model, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          model.set(key, params[key]);
+        }
+      });
+      model.save();
+      this.transitionTo('post');
     }
   }
 });
