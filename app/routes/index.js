d0928ef5fa63f73f6e8d1c56bc50cf1cf3ad3c00
Andrew Haupt & Jessica Joly <student@epicodus.com>
2015-09-24 12:14:43 -0700

    Add ember-simple-auth

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,19 @@ export default Ember.Route.extend({
       var newPost = this.store.createRecord('post', params);
       newPost.save();
       this.transitionTo('index');
+    },
+    login: function() {
+      this.get('session').authenticate('authenticator:firebase', {
+          'email': this.get('email'),
+          'password': this.get('password')
+      }).then(function() {
+          this.transitionToRoute('index');
+      }.bind(this));
+    },
+    logout: function() {
+        this.get('session').invalidate().then(function() {
+            this.transitionToRoute('login');
+        }.bind(this));
     }
   }
 });
